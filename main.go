46d28e6e1dcb382b71ea4a22b8392b2ab2d19455
thinker <thinker.gkhit@gmail.com>
2020-10-29 21:05:47 +0300

    Declare variables

--- main.go
+++ main.go
@@ -12,22 +12,32 @@ import (
 )
 
 func main() {
-	filename := filepath.Base(os.Args[0])
-	extension := filepath.Ext(filename)
-	configfilename := filename[0 : len(filename)-len(extension)]
+	var (
+		filename       string
+		extension      string
+		configfilename string
+		dir            string
+		err            error
+		configpath     string
+		opt            *service.ServiceOptions
+		svc            *service.Service
+	)
+	filename = filepath.Base(os.Args[0])
+	extension = filepath.Ext(filename)
+	configfilename = filename[0 : len(filename)-len(extension)]
 	configfilename += ".json"
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	dir, err = filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 	}
-	configpath := path.Join(dir, configfilename)
+	configpath = path.Join(dir, configfilename)
 	// configpath = "/home/thinker/projects/go/src/gkhit.ru/scada/mqcltmssvc/mqcltmssvc.json"
-	opt := service.NewOptions()
+	opt = service.NewOptions()
 	err = opt.Load(configpath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	svc := service.NewService(opt)
+	svc = service.NewService(opt)
 	svc.Start()
 }
